9ec51f5098e83a6f30e3e494e43c078d9f19fe8b
Ville Karvonen <villekarvonen1@gmail.com>
2023-12-01 10:26:21 +0200

    day 1 part 1

--- 2023/1/solution.hs
+++ 2023/1/solution.hs
@@ -1,6 +1,15 @@
 import Data.List
+import Data.Char
+import qualified Data.Text as T
+
 
 main :: IO ()
 main = do
-  filecontent <- readFile "input.txt"
-  print filecontent
+  input <- readFile "input.txt"
+
+  let digits = (filter isDigit) <$> (lines input) 
+  let firstDigits = (head <$> digits)
+  let lastDigits  = (head . reverse) <$> digits
+  let values = zipWith (\a b -> a:b:[]) firstDigits lastDigits
+  let ints = (\v -> read v :: Int) <$> values
+  print $ sum ints
