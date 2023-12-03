7dc6046afbf1960c57934af77f60cb656fe9a2db
Ville Karvonen <villekarvonen1@gmail.com>
2023-12-03 09:38:31 +0200

    day 2 part 2

--- 2023/2/solution.hs
+++ 2023/2/solution.hs
@@ -1,6 +1,24 @@
 import Data.List
 import Data.Char
 
+greensMax cs = greensMax' cs 0  
+greensMax' :: String -> Int -> Int
+greensMax' [] rslt = rslt
+greensMax' (c1:c2:' ':'g':'r':'e':'e':'n':cs) rslt = greensMax' cs (max (read (c1:c2:[]) :: Int) rslt)
+greensMax' (c:cs) rslt = greensMax' cs rslt
+
+redsMax cs = redsMax' cs 0  
+redsMax' :: String -> Int -> Int
+redsMax' [] rslt = rslt
+redsMax' (c1:c2:' ':'r':'e':'d':cs) rslt = redsMax' cs (max (read (c1:c2:[]) :: Int) rslt)
+redsMax' (c:cs) rslt = redsMax' cs rslt
+
+bluesMax cs = bluesMax' cs 0  
+bluesMax' :: String -> Int -> Int
+bluesMax' [] rslt = rslt
+bluesMax' (c1:c2:' ':'b':'l':'u':'e':cs) rslt = bluesMax' cs (max (read (c1:c2:[]) :: Int) rslt)
+bluesMax' (c:cs) rslt = bluesMax' cs rslt
+
 isLegal :: String -> Bool
 isLegal [] = True 
 isLegal (c1:c2:' ':'r':'e':'d':cs) = if (read (c1:c2:[]) :: Int) > 12 then False else isLegal cs
@@ -19,3 +37,6 @@ main = do
 
   let legalLines =  filter isLegal input
   print $ sum $ gameId <$> legalLines
+
+  let powerSum = sum $ map (\line -> (redsMax line) * (greensMax line) * (bluesMax line)) input 
+  print powerSum
