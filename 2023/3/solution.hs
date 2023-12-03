48fd89416b5470d4d43306f4ca63e285561ec3f7
Ville Karvonen <villekarvonen1@gmail.com>
2023-12-03 17:46:01 +0200

    day3 first digits of nums

--- 2023/3/solution.hs
+++ 2023/3/solution.hs
@@ -6,9 +6,22 @@ symbolIn (x, y) schematic = elem ((schematic !! y) !! x) ['-','+','*','$','#','%
 
 hasAdjacentSymbol :: (Int, Int) -> [String] -> Bool
 hasAdjacentSymbol (x,y) schematic = or $ map (\c -> symbolIn c schematic ) adjacent
-  where adjacent = filter (\(xc, yc) -> xc >= 0 && yc >= 0) [(x-1,y),(x+1,y),(x,y-1),(x,y+1),(x+1,y+1),(x-1,y-1),(x-1,y+1),(x+1,y-1)]
+  where adjacent = filter (\(xc, yc) -> xc >= 0 && yc >= 0 && yc <= 9 && xc <= 9) [(x-1,y),(x+1,y),(x,y-1),(x,y+1),(x+1,y+1),(x-1,y-1),(x-1,y+1),(x+1,y-1)]
+
+--TODODODODODODODOD
+allNumCoords :: (Int, Int) -> [String] ->[(Int, Int)]
+allNumCoords (x,y) schematic = [(x,y)]
+
+numCoords :: [String] -> [[(Int,Int)]]
+numCoords schematic = numCoords' schematic (0,0) [[]]
+
+numCoords' schematic (x, y) coords
+  | y > 9                           = coords
+  | x > 9                           = numCoords' schematic (0, y+1) coords
+  | isDigit ((schematic !! y) !! x) = numCoords' schematic (x+3, y) coords ++ [allNumCoords (x,y) schematic]
+  | otherwise                       = numCoords' schematic (x+1, y) coords
 
 main :: IO ()
 main = do
   schematic <- lines <$> readFile "input.txt"
-  print $ hasAdjacentSymbol (1, 0) schematic
+  print $ numCoords schematic
