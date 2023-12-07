6885480a0af5a951f2bafbcfa3528455662cf2aa
Ville Karvonen <villekarvonen1@gmail.com>
2023-12-07 13:08:31 +0200

    Day 4 part 2 done

--- 2023/4/solution.hs
+++ 2023/4/solution.hs
@@ -11,6 +11,11 @@ parseCard' (' ':'|':cs)       rslt = rslt
 parseCard' []                 rslt = rslt
 parseCard' (_:cs)             rslt = parseCard' cs rslt
 
+updateCards :: [Int] -> [Int] -> Int -> [Int]
+updateCards (w:ws) cards 200 = cards
+updateCards (w:ws) cards n   = updateCards ws updated (n+1)
+  where updated = zipWith (+) [if i > n && i <= n+w then cards !! n else 0 | i <- [0..200]] cards
+
 main :: IO ()
 main = do
   cards <- lines <$> readFile "input.txt"
@@ -19,7 +24,10 @@ main = do
   let myNums = map parseCard (map (\line -> dropWhile (/= '|') line ++ [' '] ) cards)
 
   let intersection = zipWith intersect myNums winNums
-  let wins = filter (>0) (map length intersection)
-  let points = map (\i -> 2^(i-1)) wins
+  let wins =  (map length intersection)
+  let points = map (\i -> 2^(i-1)) (filter (>0) wins)
   print $ sum points
   --26218
+
+  let cards = [1 | i <- [0..200]]
+  print $ sum $ updateCards wins cards 0
